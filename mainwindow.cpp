582d90a05367d6e53002fa2f0c570f55ab1f11f0
prach <prach.p59@gmail.com>
2023-01-02 14:21:27 +0300

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,80 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    connect(socket,&QTcpSocket::readyRead,this,&MainWindow::slotReadyRead);
+    connect(socket,&QTcpSocket::disconnected,this,&QTcpSocket::deleteLater);
+    nextBlockSize=0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_connect_clicked()
+{
+    socket->connectToHost("127.0.0.1",2323);
+}
+
+void MainWindow::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data,QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_10);
+    out<<quint16(0)<<QTime::currentTime()<<str;
+    out.device()->seek(0);
+    out<<quint16(Data.size()-sizeof(quint16));
+    socket->write(Data);
+    ui->message->clear();
+}
+
+void MainWindow::slotReadyRead()
+{
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_5_10);
+    if(in.status()==QDataStream::Ok)
+    {
+//        QString str;
+//        in>>str;
+//        ui->textBrowser->append(str);
+        for( ; ; )
+        {
+            if(nextBlockSize==0)
+            {
+                if(socket->bytesAvailable()<2)
+                {
+                    break;
+                }
+                in>>nextBlockSize;
+            }
+            if(socket->bytesAvailable()<nextBlockSize)
+            {
+                break;
+            }
+            QString str;
+            QTime time;
+            in>>time>>str;
+            nextBlockSize=0;
+            ui->textBrowser->append(time.toString()+" "+str);
+        }
+    }else{
+        ui->textBrowser->append("ready error");
+    }
+}
+
+void MainWindow::on_send_clicked()
+{
+    SendToServer(ui->message->text());
+}
+
+void MainWindow::on_message_returnPressed()
+{
+    SendToServer(ui->message->text());
+}
---
+++ mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QTime>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void on_connect_clicked();
+
+    void on_send_clicked();
+
+    void on_message_returnPressed();
+
+private:
+    QByteArray Data;
+    QTcpSocket* socket;
+    void SendToServer(QString str);
+    quint16 nextBlockSize;
+private:
+    Ui::MainWindow *ui;
+public slots:
+    void slotReadyRead();
+};
+#endif // MAINWINDOW_H
