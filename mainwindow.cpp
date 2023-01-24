c879ec228e25842a49a334ed105efa125ae12b2e
prach <39810187+prachpv@users.noreply.github.com>
2023-01-24 17:36:08 +0300

    6th

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,8 @@ MainWindow::MainWindow(QWidget *parent)
     nextBlockSize=0;
     bool ok;
     QString text= QInputDialog::getText(this,QString::fromUtf8("Введите имя пользователя"),QString::fromUtf8("Ваш имя:"),QLineEdit::Normal,QDir::home().dirName(),&ok);
-username=text;
+    username=text;
+
 }
 
 MainWindow::~MainWindow()
@@ -33,18 +34,80 @@ void MainWindow::on_connect_clicked()
 
 }
 
-void MainWindow::SendToServer(QString name,QString str,int mode)
+void MainWindow::SendToServer(QString name,QString str,int _mode)
 {
     Data.clear();
 
-    QDataStream out(&Data,QIODevice::WriteOnly);
-    out.setVersion(QDataStream::Qt_5_10);
-    out<<quint16(0)<<name<<mode<<QTime::currentTime()<<str;
-    out.device()->seek(0);
-    out<<quint16(Data.size()-sizeof(quint16));
-    socket->write(Data);
-    ui->message->clear();
+        QDataStream out(&Data,QIODevice::WriteOnly);
+        out.setVersion(QDataStream::Qt_5_10);
+        out<<quint16(0)<<name<<mode<<QTime::currentTime()<<str;
+        out.device()->seek(0);
+        out<<quint16(Data.size()-sizeof(quint16));
+        socket->write(Data);
+    SendFile();
+        ui->message->clear();
 }
+
+
+void MainWindow::SendFile()
+{
+    QString filePathName = file_path_temp;
+        QStringList filePath = filePathName.split("/");
+
+        QFile file(file_path_temp);
+        QByteArray q;
+        int i = 0;
+        QByteArray block;
+        QByteArray arrBlock;
+        QByteArray junk("!@#Rtasd#$%sdfs!!!)()334rrer");
+        QList<QByteArray> Blocks;
+//_mode=1;
+        if (file.open(QIODevice::ReadOnly)) {
+            QFileInfo fileInfo(file);
+            QString fileName(filePath.last());
+
+            QDataStream out(&arrBlock, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_5_7);
+            //
+
+//            out << quint16(0) << QTime::currentTime()
+//                <<username<<fileName << fileInfo.size();
+
+//            out.device()->seek(0);
+//            out << quint16(arrBlock.size() - sizeof(quint16));
+            //
+
+
+            while (!file.atEnd())
+            {
+                q = file.readAll();
+                file.close();
+            }
+    //        Blocks = q.split('\n');
+            int pos = 0, arrsize = q.size(), sizeInArray = 512;
+            while(pos<arrsize){
+                QByteArray arr = q.mid(pos, sizeInArray);
+                Blocks << arr;
+                pos+=arr.size();
+            }
+
+            foreach (block, Blocks) {
+
+                socket->write(block);
+                socket->waitForBytesWritten();
+                if (block == Blocks.last()) {
+                    qDebug() << "sended!";
+                    socket->write(junk);
+                    socket->waitForBytesWritten();
+                    block.clear();
+                }
+                q.clear();
+                i++;
+                //ui->prBr_fileSending->setValue(i/Blocks.size()*100);
+            }
+        }
+}
+
 
 void MainWindow::slotReadyRead()
 {
@@ -72,14 +135,13 @@ void MainWindow::slotReadyRead()
             QString str;
             QTime time;
             QString name;
-           int mode_sender;
+            int mode_sender;
             in>>name>>mode_sender>>time>>str;
-            str+="///"+QString::number(mode_sender);
+            //str+="///"+QString::number(mode_sender);
             nextBlockSize=0;
-            if(mode_sender==0)
-            {
-            ui->textBrowser->append(time.toString()+"["+name+"]"+" "+str);
-            }
+
+                ui->textBrowser->append(time.toString()+"["+name+"]"+" "+str);
+
         }
     }else{
         ui->textBrowser->append("ready error");
@@ -100,3 +162,19 @@ void MainWindow::on_message_returnPressed()
 {
     SendToServer(username,ui->message->text(),mode);
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, QString::fromUtf8("Имя файла"));
+
+    if(!file_name.isEmpty())
+    {
+        mode =1;
+        file_path_temp=file_name;
+        //ui->message->setEnabled(false);
+
+    }
+    //ui->message->setText(file_name);
+}
+
+
